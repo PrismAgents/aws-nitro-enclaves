d48347223789cc6b391063ac93d76482c574b29b
danielles0xg <daniel.profile.eng@gmail.com>
2025-04-01 19:29:01 -0400

    update traffic forwarder to support db call

--- resources/code/my-first-enclave/secure-local-channel/run.sh
+++ resources/code/my-first-enclave/secure-local-channel/run.sh
@@ -10,9 +10,11 @@ ip link set dev lo up
 
 # Add a hosts record, pointing target site calls to local loopback
 echo "127.0.0.1   pro-openapi.debank.com" >> /etc/hosts
+echo "127.0.0.1   3.129.1.135" >> /etc/hosts
 
 touch /app/libnsm.so
 
 # Run traffic forwarder in background and start the server
 python3 /app/traffic_forwarder.py 127.0.0.1 443 3 8001 &
+python3 /app/traffic_forwarder.py 127.0.0.1 5432 3 8002 &
 python3 /app/server.py server 5005
